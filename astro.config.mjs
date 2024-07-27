986cb64dd9a4a49250cb17f869bb3a2568255516
Capicode <caper.asdev@gmail.com>
2024-07-27 10:32:08 -0500

    project component responsive, next:add properties for dynamic component

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  base: process.env.SERVER === 'development' ? `/${process.env.REPO}` : '/',
+  build: {
+    assets: 'assets'
+  }
 });
