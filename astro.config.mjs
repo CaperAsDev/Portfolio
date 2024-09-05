851e0351af1fde1115894acfb704d4fd51e1c689
Capicode <caper.asdev@gmail.com>
2024-09-05 11:01:59 -0500

    remove compress entirely

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-import playformCompress from "@playform/compress";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://caperasdev.com",
-  integrations: [icon(), sitemap(), playformCompress(
-    {CSS: false}
-  )],
+  integrations: [icon(), sitemap()],
   base: process.env.SERVER === 'development' ? `/${process.env.REPO}` : '/',
   build: {
     assets: 'assets'
