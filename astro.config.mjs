ba87ac78977c8ee63be81c8d6ad4f1a9969d3ffe
Capicode <caper.asdev@gmail.com>
2024-11-13 11:23:41 -0500

    Before applying i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"]
+  },
   site: "https://caperasdev.com",
   integrations: [icon(), sitemap()],
   base: process.env.SERVER === 'development' ? `/${process.env.REPO}` : '/',
