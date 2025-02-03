42e5bce4d4d3f6579c298c70d2e1970f7db6d31a
Capicode <caper.asdev@gmail.com>
2025-02-03 12:21:06 -0500

    SEO meta tags added,and view transition navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,17 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://caperasdev.com",
-  integrations: [icon(), sitemap()],
+  integrations: [icon(), sitemap(
+    {
+      i18n: {
+      defaultLocale: 'es',
+      locales: {
+        en: 'en-US', 
+        es: 'es-ES',
+      },
+    },
+  }
+  )],
   base: process.env.SERVER === 'development' ? `/${process.env.REPO}` : '/',
   build: {
     assets: 'assets'
